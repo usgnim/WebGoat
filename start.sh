89db9ba9244e8d6f2cccaa172323aa8684c67805
Kim Su Min <105275495+usgnim@users.noreply.github.com>
2024-09-08 14:30:29 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
-sudo yum install -y java-17-amazon-corretto-devel
-BUILD_JAR=$(ls /home/ubuntu/build/*.jar)
-JAR_NAME=$(basename $BUILD_JAR)
-echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/deploy.log
+# sudo yum install -y java-17-amazon-corretto-devel
+# BUILD_JAR=$(ls /home/ubuntu/build/*.jar)
+# JAR_NAME=$(basename $BUILD_JAR)
+# echo "> build 파일명: $JAR_NAME" >> /home/ubuntu/deploy.log
 
-echo "> build 파일 복사" >> /home/ubuntu/deploy.log
-DEPLOY_PATH=/home/ubuntu/
-cp $BUILD_JAR $DEPLOY_PATH
+# echo "> build 파일 복사" >> /home/ubuntu/deploy.log
+# DEPLOY_PATH=/home/ubuntu/
+# cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/deploy.log
+# echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/deploy.log
+# CURRENT_PID=$(pgrep -f $JAR_NAME)
+
+# if [ -z $CURRENT_PID ]
+# then
+#   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/deploy.log
+# else
+#   echo "> kill -15 $CURRENT_PID"
+#   kill -15 $CURRENT_PID
+#   sleep 5
+# fi
+
+# DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+# echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/deploy.log
+# nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/deploy_err.log &
+JARFILE=$(ls /home/ubuntu/build/target/*.jar)
+echo "filename : $JARFILE" >> startsh.log
+
 CURRENT_PID=$(pgrep -f $JAR_NAME)
+echo "currentpid : $CURRENT_PID" >> startsh.log
+kill -9 $CURRENT_PID 2>/dev/null
+sleeop 5
 
-if [ -z $CURRENT_PID ]
-then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/deploy.log
-else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
-fi
-
-DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/deploy_err.log &
+nohup java -jar $JARFILE > /dev/null 2> /dev/null < /dev/null &
