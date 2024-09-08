bfa280bc7681aab7ee23a029d68dfe1cf89a653d
Kim Su Min <105275495+usgnim@users.noreply.github.com>
2024-09-08 21:02:53 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -26,9 +26,9 @@
 JARFILE=$(ls /home/ubuntu/build/target/*.jar)
 echo "filename : $JARFILE" >> startsh.log
 
-CURRENT_PID=$(pgrep -f $JAR_NAME)
+CURRENT_PID=$(pgrep -f $JARFILE)
 echo "currentpid : $CURRENT_PID" >> startsh.log
 kill -9 $CURRENT_PID 2>/dev/null
-sleeop 5
+sleep 5
 
 nohup java -jar $JARFILE > /dev/null 2> /dev/null < /dev/null &
